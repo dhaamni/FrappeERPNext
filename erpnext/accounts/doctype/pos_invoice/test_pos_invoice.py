153df4eca5901ddee6fdfcf019c4d3e2983bf07c
ljain112 <ljain112@gmail.com>
2025-07-13 18:16:49 +0530

    chore: resolve conflicts

--- erpnext/accounts/doctype/pos_invoice/test_pos_invoice.py
+++ erpnext/accounts/doctype/pos_invoice/test_pos_invoice.py
@@ -1071,17 +1071,3 @@ def create_pos_invoice(**args):
 		pos_inv.payment_schedule = []
 
 	return pos_inv
-
-
-<<<<<<< HEAD
-def make_batch_item(item_name):
-	from erpnext.stock.doctype.item.test_item import make_item
-
-	if not frappe.db.exists(item_name):
-		return make_item(item_name, dict(has_batch_no=1, create_new_batch=1, is_stock_item=1))
-=======
-def set_allow_partial_payment(pos_profile, value):
-	pos_profile.reload()
-	pos_profile.allow_partial_payment = value
-	pos_profile.save()
->>>>>>> de8c3ba968 (chore: fix flacky test and remove redundant code)
