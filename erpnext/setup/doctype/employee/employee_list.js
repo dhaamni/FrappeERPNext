51790be3339d29f710039afa6092a27445a382a2
barredterra <14891507+barredterra@users.noreply.github.com>
2025-07-14 14:52:33 +0200

    refactor(Employee): get_indicator

--- erpnext/setup/doctype/employee/employee_list.js
+++ erpnext/setup/doctype/employee/employee_list.js
@@ -2,12 +2,10 @@ frappe.listview_settings["Employee"] = {
 	add_fields: ["status", "branch", "department", "designation", "image"],
 	filters: [["status", "=", "Active"]],
 	get_indicator: function (doc) {
-		var indicator = [
+		return [
 			__(doc.status, null, "Employee"),
-			frappe.utils.guess_colour(doc.status),
+			{ Active: "green", Inactive: "red", Left: "gray", Suspended: "orange" }[doc.status],
 			"status,=," + doc.status,
 		];
-		indicator[1] = { Active: "green", Inactive: "red", Left: "gray", Suspended: "orange" }[doc.status];
-		return indicator;
 	},
 };
