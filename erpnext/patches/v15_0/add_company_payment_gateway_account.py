cc64ac6bfd0cea480671b5bd9889e3a847442798
ravibharathi656 <ravibharathi656@gmail.com>
2025-07-16 23:50:52 +0530

    refactor: use gateway_account instead of pgc

--- erpnext/patches/v15_0/add_company_payment_gateway_account.py
+++ erpnext/patches/v15_0/add_company_payment_gateway_account.py
@@ -2,6 +2,6 @@ import frappe
 
 
 def execute():
-	for pgc in frappe.get_list("Payment Gateway Account", fields=["name", "payment_account"]):
-		company = frappe.db.get_value("Account", pgc.payment_account, "company")
-		frappe.db.set_value("Payment Gateway Account", pgc.name, "company", company)
+	for gateway_account in frappe.get_list("Payment Gateway Account", fields=["name", "payment_account"]):
+		company = frappe.db.get_value("Account", gateway_account.payment_account, "company")
+		frappe.db.set_value("Payment Gateway Account", gateway_account.name, "company", company)
