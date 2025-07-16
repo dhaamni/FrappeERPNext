6fc08f7f9d3e7afd2b9e6c8f44867eba9152ae58
l0gesh29 <logeshperiyasamy24@gmail.com>
2025-07-17 00:42:34 +0530

    fix: ignore period closing voucher

--- erpnext/accounts/report/cash_flow/cash_flow.py
+++ erpnext/accounts/report/cash_flow/cash_flow.py
@@ -6,7 +6,9 @@ from datetime import timedelta
 
 import frappe
 from frappe import _
+from frappe.query_builder import DocType
 from frappe.utils import cstr, flt
+from pypika import Order
 
 from erpnext.accounts.report.financial_statements import (
 	get_columns,
@@ -303,6 +305,7 @@ def get_opening_balance(company, period_list, filters):
 
 	cash_value = {}
 	account_types = get_cash_flow_accounts()
+	net_profit_loss = 0.0
 
 	local_filters = deepcopy(filters)
 	local_filters.start_date, local_filters.end_date = get_opening_range_using_fiscal_year(
@@ -314,7 +317,7 @@ def get_opening_balance(company, period_list, filters):
 		cash_value.setdefault(section_name, 0.0)
 
 		if section_name == "Operations":
-			cash_value[section_name] += get_net_income(company, period_list, local_filters)
+			net_profit_loss += get_net_income(company, period_list, local_filters)
 
 		for account in section.get("account_types", []):
 			account_type = account.get("account_type")
@@ -323,18 +326,18 @@ def get_opening_balance(company, period_list, filters):
 			amount = get_account_type_based_gl_data(company, local_filters) or 0.0
 
 			if account_type == "Depreciation":
-				cash_value[section_name] += -amount
+				cash_value[section_name] += amount * -1
 			else:
 				cash_value[section_name] += amount
 
-	return sum(cash_value.values())
+	return sum(cash_value.values()) + net_profit_loss
 
 
 def get_net_income(company, period_list, filters):
-	gl_entries_by_account_for_income = {}
-	gl_entries_by_account_for_expense = {}
+	gl_entries_by_account_for_income, gl_entries_by_account_for_expense = {}, {}
 	income, expense = 0.0, 0.0
 	from_date, to_date = get_opening_range_using_fiscal_year(company, period_list)
+
 	for root_type in ["Income", "Expense"]:
 		for root in frappe.db.sql(
 			"""select lft, rgt from tabAccount
@@ -353,17 +356,20 @@ def get_net_income(company, period_list, filters):
 				root.lft,
 				root.rgt,
 				root_type=root_type,
+				ignore_closing_entries=True,
 			)
 
 	for entries in gl_entries_by_account_for_income.values():
 		for entry in entries:
 			if entry.posting_date <= to_date:
-				income += (flt(entry.debit, 3) - flt(entry.credit, 3)) * -1
+				amount = (entry.debit - entry.credit) * -1
+				income = flt((income + amount), 2)
 
 	for entries in gl_entries_by_account_for_expense.values():
 		for entry in entries:
 			if entry.posting_date <= to_date:
-				expense += flt(entry.debit, 3) - flt(entry.credit, 3)
+				amount = entry.debit - entry.credit
+				expense = flt((expense + amount), 2)
 
 	return income - expense
 
@@ -373,18 +379,24 @@ def get_opening_range_using_fiscal_year(company, period_list):
 	previous_day = first_from_date - timedelta(days=1)
 
 	# Get the earliest fiscal year for the company
-	earliest_fy = frappe.db.get_all(
-		"Fiscal Year",
-		filters={"company": company},
-		fields=["year_start_date"],
-		order_by="year_start_date asc",
-		limit=1,
-	)
 
-	if earliest_fy:
-		company_start_date = earliest_fy[0]["year_start_date"]
-	else:
-		company_start_date = None
+	FiscalYear = DocType("Fiscal Year")
+	FiscalYearCompany = DocType("Fiscal Year Company")
+
+	earliest_fy = (
+		frappe.qb.from_(FiscalYear)
+		.join(FiscalYearCompany)
+		.on(FiscalYearCompany.parent == FiscalYear.name)
+		.select(FiscalYear.year_start_date)
+		.where(FiscalYearCompany.company == company)
+		.orderby(FiscalYear.year_start_date, order=Order.asc)
+		.limit(1)
+	).run(as_dict=True)
+
+	if not earliest_fy:
+		frappe.throw(_("Not able to find the earliest Fiscal Year for the given company."))
+
+	company_start_date = earliest_fy[0]["year_start_date"]
 	return company_start_date, previous_day
 
 
