59186de007c286e5425b79b695469333024fc761
Mihir Kandoi <kandoimihir@gmail.com>
2025-07-31 21:01:55 +0530

    fix: remove incorrect references from stock entry dashboard/connections tab
    (cherry picked from commit 6d82e3cc2847186a16159268b1dbd7e04eca2191)
    
    # Conflicts:
    #	erpnext/stock/doctype/stock_entry/stock_entry_dashboard.py

--- erpnext/stock/doctype/stock_entry/stock_entry_dashboard.py
+++ erpnext/stock/doctype/stock_entry/stock_entry_dashboard.py
@@ -8,12 +8,8 @@ def get_data():
 		"non_standard_fieldnames": {
 			# "DocType Name": "Reference field name",
 		},
-		"internal_links": {
-			"Purchase Order": ["items", "purchase_order"],
-			"Subcontracting Order": ["items", "subcontracting_order"],
-			"Subcontracting Receipt": ["items", "subcontracting_receipt"],
-		},
 		"transactions": [
+<<<<<<< HEAD
 			{
 				"label": _("Reference"),
 				"items": [
@@ -22,5 +18,8 @@ def get_data():
 					"Subcontracting Receipt",
 				],
 			},
+=======
+			{"label": _("Stock Reservation"), "items": ["Stock Reservation Entry"]},
+>>>>>>> 6d82e3cc28 (fix: remove incorrect references from stock entry dashboard/connections tab)
 		],
 	}
