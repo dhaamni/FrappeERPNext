a11d368465b779dcc2b4c155d5c75965d89bc769
rohitwaghchaure <rohitw1991@gmail.com>
2025-08-07 16:15:31 +0530

    chore: fix conflicts

--- erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/test_sales_invoice.py
@@ -7,13 +7,8 @@ import json
 import frappe
 from frappe import qb
 from frappe.model.dynamic_links import get_dynamic_link_map
-<<<<<<< HEAD
 from frappe.tests.utils import FrappeTestCase, change_settings
-from frappe.utils import add_days, flt, format_date, getdate, nowdate, today
-=======
-from frappe.tests import IntegrationTestCase, change_settings
 from frappe.utils import add_days, cint, flt, format_date, getdate, nowdate, today
->>>>>>> c8410cb5ca (fix: zero valuation rate for the batch)
 
 import erpnext
 from erpnext.accounts.doctype.account.test_account import create_account, get_inventory_account
@@ -4580,61 +4575,6 @@ class TestSalesInvoice(FrappeTestCase):
 		self.assertEqual(stock_ledger_entry.qty, 2.0)
 		self.assertEqual(stock_ledger_entry.stock_value_difference, 0.0)
 
-<<<<<<< HEAD
-=======
-	def test_system_generated_exchange_gain_or_loss_je_after_repost(self):
-		from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_entry
-		from erpnext.accounts.doctype.repost_accounting_ledger.test_repost_accounting_ledger import (
-			update_repost_settings,
-		)
-
-		update_repost_settings()
-
-		si = create_sales_invoice(
-			customer="_Test Customer USD",
-			debit_to="_Test Receivable USD - _TC",
-			currency="USD",
-			conversion_rate=80,
-		)
-
-		pe = get_payment_entry("Sales Invoice", si.name)
-		pe.reference_no = "10"
-		pe.reference_date = nowdate()
-		pe.paid_from_account_currency = si.currency
-		pe.paid_to_account_currency = "INR"
-		pe.source_exchange_rate = 85
-		pe.target_exchange_rate = 1
-		pe.paid_amount = si.outstanding_amount
-		pe.insert()
-		pe.submit()
-
-		ral = frappe.new_doc("Repost Accounting Ledger")
-		ral.company = si.company
-		ral.append("vouchers", {"voucher_type": si.doctype, "voucher_no": si.name})
-		ral.save()
-		ral.submit()
-
-		je = frappe.qb.DocType("Journal Entry")
-		jea = frappe.qb.DocType("Journal Entry Account")
-		q = (
-			(
-				frappe.qb.from_(je)
-				.join(jea)
-				.on(je.name == jea.parent)
-				.select(je.docstatus)
-				.where(
-					(je.voucher_type == "Exchange Gain Or Loss")
-					& (jea.reference_name == si.name)
-					& (jea.reference_type == "Sales Invoice")
-					& (je.is_system_generated == 1)
-				)
-			)
-			.limit(1)
-			.run()
-		)
-
-		self.assertEqual(q[0][0], 1)
-
 	def test_non_batchwise_valuation_for_moving_average(self):
 		from erpnext.stock.doctype.item.test_item import make_item
 
@@ -4728,7 +4668,6 @@ class TestSalesInvoice(FrappeTestCase):
 
 		doc.db_set("do_not_use_batchwise_valuation", original_value)
 
->>>>>>> c8410cb5ca (fix: zero valuation rate for the batch)
 
 def make_item_for_si(item_code, properties=None):
 	from erpnext.stock.doctype.item.test_item import make_item
